54d76cb8e54aa8858f46159dd4f8a5cedf643787
divinejoshua <divineuche399@gmail.com>
2022-08-14 18:36:46 +0100

    Started editing login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="login-page">
     <h1>This is the login page</h1>
+
+    <form @submit.prevent="" novalidate>
+
+      <div>
+        <input type="email" class="form-control" placeholder="Email">
+        <input type="password" class="form-control" placeholder="Password">
+        <button type="submit" class="form-control-btn">Login</button>
+      </div>
+
+    </form>
     <router-link to="/">Home</router-link>
   </div>
 </template>
