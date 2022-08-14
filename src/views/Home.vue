6253c079bc0d442e70f49091cd97be7ff9487e4c
divinejoshua <divineuche399@gmail.com>
2022-08-14 18:06:50 +0100

    Started adding css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="page">
+    <div class="header">
+      <div class="brand">Vue JWT</div>
+      <div class="logout">Logout</div>
+    </div>
+    <div class="body">
+      <div class="username">@divine</div>
+      <div class="email">divine@gmail.com</div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
---
+++ src/views/Login.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="login-page">
+    <h1>This is the login page</h1>
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
