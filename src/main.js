d12e6c5c023ba6a14d2eb734171d909be88e5c77
divinejoshua <divineuche399@gmail.com>
2022-08-14 19:13:54 +0100

    Made changes

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
+
+// Add axios defaults 
+axios.defaults.baseURL = "http://127.0.0.1:8000/"
+
+// Axios credentials 
+axios.defaults.withCredentials = true
+
 createApp(App).use(router).mount('#app')
