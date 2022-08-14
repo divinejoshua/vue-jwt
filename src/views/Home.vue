1ed953187f17fb4082c636ac8523f5ff59fd5d9b
divinejoshua <divineuche399@gmail.com>
2022-08-14 18:23:25 +0100

    Added comments to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="page">
+
+    <!-- Header  -->
     <div class="header">
       <div><span class="vue brand">Vue JWT</span> </div>
       <div class="logout">Logout</div>
     </div>
+
+    <!-- Body  -->
     <div class="body">
       <div class="username">divine</div>
       <div class="email">divine@gmail.com</div>
     </div>
+
+    
   </div>
 </template>
 
