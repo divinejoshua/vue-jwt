efb6a57d146043f4de5d98d336458a5175afeb5e
divinejoshua <divineuche399@gmail.com>
2022-08-14 18:18:02 +0100

    Added more css styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="page">
     <div class="header">
-      <div class="brand"><span class="vue">Vue</span> JWT</div>
+      <div><span class="vue brand">Vue</span><span class="brand">JWT</span> </div>
       <div class="logout">Logout</div>
     </div>
     <div class="body">
