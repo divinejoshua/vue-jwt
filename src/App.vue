baaf3cec53576745a1230b3bc37e2a5b7cb08020
divinejoshua <divineuche399@gmail.com>
2022-08-18 23:09:59 +0100

    Made changes

--- src/App.vue
+++ src/App.vue
@@ -25,17 +25,39 @@ import axios from 'axios'
 export default {
   created: function () {
 
+    
+
     // check for 401 unauthorized axios errors and recalls the attempt token method in vuex
-  axios.interceptors.response.use((response) => {
-      return response
-    }, (error) => {
-      if (error.response.status == 401) {
-         if (store.state.access_token) {
-            store.dispatch('refreshAccessToken') 
-         }
-      }
-      return Promise.reject(error);
-    })
+  // axios.interceptors.response.use((response) => {
+  //     return response
+  //   }, (error) => {
+  //     if (error.response.status == 401) {
+  //        if (store.state.access_token) {
+  //           store.dispatch('refreshAccessToken').then(() =>{
+  //             console.log("error")
+  //             axios.request(error.config);
+  //           })
+
+  //        }
+  //     }
+  //     return Promise.reject(error);
+  //   })
+
+            let tried = true
+
+          axios.interceptors.response.use(null, (error) => {
+            if(tried){
+                    tried = false
+                if (error.config && error.response && error.response.status === 401) {
+                  return store.dispatch('refreshAccessToken').then(() => {
+                    error.config.headers.Authorization = store.state.access_token
+                    return axios.request(error.config);
+                  });
+                }
+
+                return Promise.reject(error);
+            }
+        });
     
   //     //  Refresh for access token every 1 minute
   //    setInterval(function() {
