895e70111c7c61b9e515e835876658f4e86e4d0f
divinejoshua <divineuche399@gmail.com>
2022-08-18 22:00:29 +0100

    Made changes

--- src/App.vue
+++ src/App.vue
@@ -18,18 +18,32 @@
 
 <script>
 import { store } from '@/store'
+import axios from 'axios'
+
 
 
 export default {
   created: function () {
-    
-      //  Refresh for access token every 1 minute
-     setInterval(function() {
-      if(store.state.access_token){
-          store.dispatch('refreshAccessToken') 
+
+    // check for 401 unauthorized axios errors and recalls the attempt token method in vuex
+  axios.interceptors.response.use((response) => {
+      return response
+    }, (error) => {
+      if (error.response.status == 401) {
+         if (store.state.access_token) {
+            store.dispatch('refreshAccessToken') 
+         }
       }
-    },
-   2 * 60 * 1000);
+      return Promise.reject(error);
+    })
+    
+  //     //  Refresh for access token every 1 minute
+  //    setInterval(function() {
+  //     if(store.state.access_token){
+  //         store.dispatch('refreshAccessToken') 
+  //     }
+  //   },
+  //  2 * 60 * 1000);
 
 
 
