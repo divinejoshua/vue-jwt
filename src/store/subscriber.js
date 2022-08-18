6f4e106be114dc782383f3cf9859016aefa9071a
divinejoshua <divineuche399@gmail.com>
2022-08-18 23:29:22 +0100

    Made changes

--- src/store/subscriber.js
+++ src/store/subscriber.js
@@ -5,7 +5,7 @@ store.subscribe((mutation) => {
     if(mutation.type=="SET_ACCESS_TOKEN" && mutation.payload){
 
         // Set axios authorisation headers 
-        axios.defaults.headers.common['Authorization'] = `Bearer ${mutation.payload}`
+        axios.defaults.headers.common['Authorization'] = `Bearer X ${mutation.payload}`
     } else {
         axios.defaults.headers.common['Authorization'] = null
     }
