4e9d1c80f81800d9845a40ec08364de100d825e9
divinejoshua <divineuche399@gmail.com>
2022-08-18 23:34:43 +0100

    Made changes

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ export default {
       // Axios interceptors
       axios.interceptors.response.use(null, (error) => {
             if (error.config && error.response && error.response.status === 401 && store.state.access_token) {
-              return store.dispatch('refreshAccessToken').then(() => {
+              return store.dispatch('getAccessToken').then(() => {
                 error.config.headers.Authorization = `Bearer ${store.state.access_token}`
                 return axios.request(error.config);
               });
