844a6bd2691e454f911f302d049e2943ab85fe29
divinejoshua <divineuche399@gmail.com>
2022-08-18 18:06:46 +0100

    Commented out withCredentials

--- src/main.js
+++ src/main.js
@@ -8,6 +8,6 @@ import axios from 'axios'
 axios.defaults.baseURL = "http://127.0.0.1:8000/"
 
 // Axios credentials 
-axios.defaults.withCredentials = true
+// axios.defaults.withCredentials = true
 
 createApp(App).use(router).mount('#app')
