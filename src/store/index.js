5a5f3500ca9c33b705df7219ece03479578161b9
divinejoshua <divineuche399@gmail.com>
2022-08-19 14:35:43 +0100

    Made changes

--- src/store/index.js
+++ src/store/index.js
@@ -30,8 +30,8 @@ export const store = createStore({
     async getAccessToken({ commit }){
  
       try{
-          // let response = await axios.post('accounts/auth/token/refresh/', {'refresh': localStorage.getItem('refresh')})
-          let response = await axios.post('accounts/auth/token/refresh/')
+          let response = await axios.post('accounts/auth/token/refresh/', {'refresh': localStorage.getItem('refresh')})
+          // let response = await axios.post('accounts/auth/token/refresh/')
           commit('SET_ACCESS_TOKEN', response.data.access)
 
       } catch(e){
